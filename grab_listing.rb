24d004c1076e5d6d01618726f34e888b4732ee19
Ben Peters <ben@aiqinvestments.com>
2013-08-13 18:24:48 -0600

    super basic version of basic code to grab fields of interest from a CL posting using nokogiri

---
+++ grab_listing.rb
@@ -0,0 +1,47 @@
+require 'rest-client'
+require 'nokogiri'
+require 'fileutils'
+
+url = 'http://saltlakecity.craigslist.org/apa/3990520051.html'
+if page = RestClient.get(url)
+  puts "\nGot #{url} successfully!"
+  File.open("data/cl-#{url.match(/[^\/]*$/)}", 'w') { |f| f.write page.body }
+  npage = Nokogiri::HTML(page)
+
+  description = npage.css('#postingbody')
+
+  # Process CL-id'd data
+  cltags = npage.css('.cltags')
+  address = cltags.css('.mapaddress')
+  cl_data = cltags.css('.blurbs li')
+  cl_tags_of_interest = { 'purrr' => 'cat_friendly', 'wooof' => 'dog_friendly', 'Location' => 'cl_location' }
+  meta = Hash.new
+  cl_data.each do |data|
+    cl_tags_of_interest.each_key do |key|
+      if !data.content.match(key).nil?
+        if d = data.content.match(/#{key}: (.*)/)
+          meta[cl_tags_of_interest[key]] = d.captures[0]
+        else
+          meta[cl_tags_of_interest[key]] = true
+        end
+      end
+    end
+  end
+
+  # Grab posting meta-data
+  post_info = npage.css('div.postinginfos p')
+  field_names = { 'Posting ID' => 'cl_id', 'Posted' => 'post_date', 'Updated' => 'updated_date' }
+  post_info.each do |data|
+    if key = data.content.match(/([^:]*):\ (.*)/) and field_names.has_key?(key.captures[0])
+      meta[field_names[key.captures[0]]] = key.captures[1]
+    end
+  end
+  
+  puts "\n\nRecord Parsed:"
+  puts "Description: #{description[0].content.strip()}"
+  puts "Address: #{address[0].content.split("\n")[0].strip()}"
+  puts "Metadata:"
+  meta.each {|k, v| puts "  *#{k}: #{v}" }
+else
+  puts "Problem fetching #{url}"
+end
