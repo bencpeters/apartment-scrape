d94f7ed90f62a19ff255b61939dd3c4e7c9bc2b6
Ben Peters <ben@aiqinvestments.com>
2013-08-16 10:29:08 -0600

    abstracted cl-specific logic to a separate, configurable module

--- grab_listing.rb
+++ grab_listing.rb
@@ -1,57 +1,27 @@
 require 'rest-client'
 require 'nokogiri'
 require 'fileutils'
+require_relative './lib/cl_post_parse'
 
 url = 'http://saltlakecity.craigslist.org/apa/3990520051.html'
+url = 'http://saltlakecity.craigslist.org/apa/3986668275.html'
 if page = RestClient.get(url)
   puts "\nGot #{url} successfully!"
   File.open("data/cl-#{url.match(/[^\/]*$/)}", 'w') { |f| f.write page.body }
   npage = Nokogiri::HTML(page)
 
-  description = npage.css('#postingbody')
-
   # Grab thumbs/images
-  image_links = npage.css('#thumbs a')
-  image_names = Array.new
-  image_links.each do |link|
-    image = RestClient.get(link['href'])
-    filename = "img-#{link['href'].match(/[^\/]*$/)}"
-    File.open("data/#{filename}", 'w') { |f| f.write image }
-    image_names.push(filename)
-  end
-
-  # Process CL-id'd data
-  cltags = npage.css('.cltags')
-  address = cltags.css('.mapaddress')
-  cl_data = cltags.css('.blurbs li')
+  image_names = CLPostParse.get_images(npage)
+  description = CLPostParse.get_description(npage)
+  address = CLPostParse.get_address(npage)
   cl_tags_of_interest = { 'purrr' => 'cat_friendly', 'wooof' => 'dog_friendly', 'Location' => 'cl_location' }
-  meta = Hash.new
-  meta['images'] = image_names unless image_names.empty?
-  meta['url'] = url
-  cl_data.each do |data|
-    cl_tags_of_interest.each_key do |key|
-      if !data.content.match(key).nil?
-        if d = data.content.match(/#{key}: (.*)/)
-          meta[cl_tags_of_interest[key]] = d.captures[0]
-        else
-          meta[cl_tags_of_interest[key]] = true
-        end
-      end
-    end
-  end
+  fieldnames = { 'Posting ID' => 'cl_id', 'Posted' => 'post_date', 'Updated' => 'updated_date' }.merge(cl_tags_of_interest)
+  meta = CLPostParse.get_metadata(npage, fieldnames)
+  meta['images'] = image_names
 
-  # Grab posting meta-data
-  post_info = npage.css('div.postinginfos p')
-  field_names = { 'Posting ID' => 'cl_id', 'Posted' => 'post_date', 'Updated' => 'updated_date' }
-  post_info.each do |data|
-    if key = data.content.match(/([^:]*):\ (.*)/) and field_names.has_key?(key.captures[0])
-      meta[field_names[key.captures[0]]] = key.captures[1]
-    end
-  end
-  
   puts "\n\nRecord Parsed:"
-  puts "Description: #{description[0].content.strip()}"
-  puts "Address: #{address[0].content.split("\n")[0].strip()}"
+  puts "Description: #{description}"
+  puts "Address: #{address}"
   puts "Metadata:"
   meta.each {|k, v| puts "  *#{k}: #{v}" }
 else
---
+++ lib/cl_post_parse.rb
@@ -0,0 +1,69 @@
+# Grabs a specific craigslist posting
+require 'rest-client'
+require 'nokogiri'
+require 'fileutils'
+
+module CLPostParse
+  extend self
+
+  def configure
+  end
+
+  # Grabs all images out of a post and saves them to disk.
+  # Returns a List of file names corresponding to saved images
+  def get_images(npage)
+    image_links = npage.css('#thumbs a')
+    image_names = Array.new
+    image_links.each do |link|
+      image = RestClient.get(link['href'])
+      filename = "img-#{link['href'].match(/[^\/]*$/)}"
+      save_image(image, filename)
+      image_names.push(filename)
+    end
+    return image_names
+  end
+
+  def get_description(npage)
+    npage.css('#postingbody')[0].content.strip()
+  end
+
+  def get_address(npage)
+    npage.css('.cltags .mapaddress')[0].content.split("\n")[0].strip()
+  end
+
+  def get_metadata(npage, fields)
+    meta = Hash.new
+    npage.css('.cltags .blurbs li').each do |data|
+      fields.each_key do |key|  
+        unless data.content.match(key).nil? 
+          if d = data.content.match(/#{key}: (.*)/)
+            meta[fields[key]] = d.captures[0] 
+          else
+            meta[fields[key]] = true
+          end
+        end
+      end
+    end
+    
+    post_info = npage.css('div.postinginfos p')
+    post_info.each do |data|
+      if key = data.content.match(/([^:]*):\ (.*)/) and fields.has_key?(key.captures[0])
+        meta[fields[key.captures[0]]] = key.captures[1]
+      end
+    end
+    meta
+  end
+  
+  private
+
+  # Saves an image using the configured @file_module
+  # If this module hasn't been configured, uses a 
+  # regular filesystem write
+  def save_image(image, name)
+    if @file_module
+      @file_module.save(name, image)
+    else
+      File.open("data/#{name}", 'w') { |f| f.write image }
+    end
+  end
+end
