59c11d8db9897de432a1a5f8d7e14a4f5790be21
Ben <ben@aiqinvestments.com>
2013-10-25 08:22:17 -0600

    finished tests for get_images

--- spec/cl_post_parse_spec.rb
+++ spec/cl_post_parse_spec.rb
@@ -22,6 +22,55 @@ describe CLPostParse do
             it "gets all images" do
                 images = CLPostParse.get_images(@listing)
                 images.should have(7).items
+                images.each { |x| expect(x).to match(/^[a-zA-Z0-9_\-]*\.jpg$/) }
+            end
+        end
+
+        context "with a valid post with images that don't exist" do
+            before :all do
+                path = './spec/resources/cl_post_bad_images.html'
+                @bad_img_listing = Nokogiri::HTML(open(path))
+            end
+
+            it "returns an empty array" do
+                images = CLPostParse.get_images(@bad_img_listing)
+                images.should be_empty
+            end
+        end
+
+        context "with a valid post without images" do
+            before :all do
+                path = './spec/resources/cl_post_no_images.html'
+                @no_img_listing = Nokogiri::HTML(open(path))
+            end
+
+            it "returns an empty array" do
+                images = CLPostParse.get_images(@no_img_listing)
+                images.should be_empty
+            end
+        end
+
+        context "with an invalid post" do
+            before :all do
+                url = 'http://www.google.com'
+                @google_npage = Nokogiri::HTML(RestClient.get(url))
+                @nil_npage = Nokogiri::HTML(nil)
+            end
+
+            it "returns an empty array for Google" do
+                images = CLPostParse.get_images(@google_npage)
+                images.should be_empty
+            end
+            it "returns an empty array for nil" do
+                images = CLPostParse.get_images(@nil_npage)
+                images.should be_empty
+            end
+        end
+
+        context "with a nil post" do
+            it "returns an empty array" do
+                images = CLPostParse.get_images(nil)
+                images.should be_empty
             end
         end
     end
